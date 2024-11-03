af7cefbf5c55d58aa51f52d525bfb9d17f443e76
krojerLukas <lukas.krojer@gmx.de>
2024-11-03 20:17:37 +0100

    finish food matrix sync

--- Food.js
+++ Food.js
@@ -1,11 +1,9 @@
 class Food {
-    constructor(canvas, ctx, row, col) {
+    constructor(canvas, ctx) {
         this.canvas = canvas; // canvas from 'Game.js'
         this.ctx = ctx; // ctx from 'Game.js'
         this.appleWidth = 50; // one cell wide
         this.appleHeight = 50; // on cell wide
-        this.row = row; // row position
-        this.col = col; // column position
     }
 
 
@@ -13,12 +11,17 @@ class Food {
     generateSpawnPosition () {
         this.row = Math.floor(Math.random()* 10);
         this.col = Math.floor(Math.random()* 10);
+        this.returnSpawnPosition(this.row, this.col)
+    }
+
+    returnSpawnPosition() {
+        return [this.row, this.col]
     }
 
 
     draw () {
         this.ctx.beginPath();
-        this.ctx.rect(this.row*50,this.col*50, this.appleWidth, this.appleHeight);
+        this.ctx.rect(this.col*50,this.row*50, this.appleWidth, this.appleHeight);
         this.ctx.fillStyle = "#556b2f";
         this.ctx.fill();
     }
--- Matrix.js
+++ Matrix.js
@@ -1,18 +1,43 @@
 class Matrix {
-    constructor (rows) {
-        this.rows= rows;
-        this.initializeMatrix();
+    constructor(rows, food) {
+        this.rows = rows;
+        this.food = food;
 
+        // initializes Matrix
+        this.matrix = new Array(10).fill(0).map(() => new Array(10).fill(0)); // creates an array out element in the fill() method
     }
 
 
-    // ---> cleared matrix, every element is 0. Happens only when initialized
-    initializeMatrix () {
-        this.matrix = new Array(this.rows).fill(0).map(() => new Array(this.rows).fill(0)); // creates an array out element in the fill() method
+    manageCurrentFramePositions(positions) {
+        this.tempMatrix = this.matrix;
+
+        positions.forEach((position) => {
+            this.managePosition(position)
+        })
+
+        this.matrix = this.tempMatrix;
+    }
+
+    managePosition(position) {
+        let matrixRow = position[0];
+        let matrixCol = position[1];
+
+        this.tempMatrix[matrixRow][matrixCol] = 1; // updates matrix on position 'matrixRow' and 'matrixCol'
     }
 
 
+    // ---> gets current positions of all elements of this current frame
+    getCurrentFramePositions() {
+        this.foodPosition = this.food.returnSpawnPosition()
 
+        const allPositions = [this.foodPosition]
+        this.manageCurrentFramePositions(allPositions)
+    }
+
+    // ---> updates matrix by current frame positions
+    updateMatrix() {
+        this.getCurrentFramePositions();
+    }
 
 
 }
